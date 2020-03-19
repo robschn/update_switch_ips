0fee5f84413a3536d740a17e1c713bc1b1b88a47
Robert Schneider <robmschn@gmail.com>
2020-03-19 12:51:16 -0400

    added IPAM update

--- update.py
+++ update.py
@@ -1,7 +1,9 @@
+# delete Old IPAM entry with DNS
+
 # set hostname and IP for switch in ClearPass
 
 # log into switch and change hostname and TACAS info
 
-# change IP address
+# Change name in IPAM
 
 # check to see you can still log in
